495940a0ae491028604cf82487108c878c8a3096
SeungJoonChoi <tmdwns0845@naver.com>
2022-07-04 15:58:22 +0900

    220704

--- main.c
+++ main.c
@@ -1,5 +1,45 @@
 #include "pipex.h"
 
+int openfile(char *file_or_limiter, int mode)
+{
+    if (mode == INFILE)
+    {
+        if (access(file_or_limiter, F_OK))
+        {
+            exit_with_err();
+        }
+        else
+            return (open(file_or_limiter, O_RDONLY));
+    }
+    else if (mode == OUTFILE)
+        return (open(file_or_limiter, O_WRONLY | O_CREAT | O_TRUNC, 0644));
+    else if (mode == HEREIN)
+        return (here_doc(file_or_limiter));
+    else if (mode == HEREOUT)
+        return (open(file_or_limiter, O_WRONLY | O_CREAT | O_APPEND, 0644));
+}
+
+void piping(char *cmd, char *envp[])
+{
+    pid_t pid;
+    int fd[2];
+
+    pipe(fd);
+    pid = fork();
+    if (!pid)
+    {
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        do_cmd(cmd, envp);
+    }
+    else
+    {
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        waitpid(pid, NULL, 0);
+    }
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
     int infile;
